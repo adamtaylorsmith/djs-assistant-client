70f8b6c4e08546015b39b160529df32c92587ac6
adamtaylorsmith <adamtaylor@icloud.com>
2020-12-07 09:18:42 -0500

    12-7 morning

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -2,9 +2,9 @@ let APIURL = '';
 
 switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
-        APIURL = 'http://localhost:3000';
+        APIURL = 'http://localhost:5435';
         break;
-    case 'djs-assistant-b.herokuapp.com':
+    case 'djs-assistant-c.herokuapp.com':
         APIURL = 'https://djs-assistant-b.herokuapp.com'
 };
 
